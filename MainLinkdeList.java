a037aba805d9fd680656bc2f6a552466e43f03cd
ahmedSadoun <ahmedsaadoun866@gmail.com>
2022-11-23 15:18:54 +0200

    end of the project

--- MainLinkdeList.java
+++ MainLinkdeList.java
@@ -5,14 +5,14 @@ public class MainLinkdeList {
     ShoppingCartList cart;
 
     public ItemNode searchItem(String name, String id) {
-        StoreNode store = head;
+        StoreNode store = head; // copy the head into store
         if (head == null) {
-            return null;
+            return null;// this means that there are no stores in the mall
         }
-        while (true) {
+        while (true) { // this loop will break conditionally
             // storeLinkedList that holds all of the items inside each store
             StoreLinkedList storeLinkedList = store.store;
-            if (storeLinkedList == null) {
+            if (storeLinkedList == null) { // this means that i have a store without any items
                 store = store.next;
                 continue;
             }
@@ -28,9 +28,9 @@ public class MainLinkdeList {
         }
         while (true) {
             // storeLinkedList that holds all of the items inside each store
-            StoreLinkedList storeLinkedList = store.store;
+            StoreLinkedList storeLinkedList = store.store;// holds a list of items of each store
             if (storeLinkedList == null) {
-                store = store.next;
+                store = store.next;// add the next of the current node and continue;
                 continue;
             }
             itm.storeNumber = storeNumber;
@@ -44,16 +44,16 @@ public class MainLinkdeList {
         if (cart == null) {
             return totalPrice; // means that nothing is the cart
         }
-        ItemNode head;
+        ItemNode cartHeadNode;
         while (true) {
-            head = cart.head;
-            if (head == null) {
+            cartHeadNode = cart.head;
+            if (cartHeadNode == null) {
                 return totalPrice;
             }
-            totalPrice += head.price * head.quantity; // accumulate the item price on the totalPrice
-            head = head.next; // get the next of the current itmNode
-            if (head.next == null) { // if the next is null, then it is a tail, get its value and break
-                totalPrice += head.price * head.quantity;
+            totalPrice += cartHeadNode.price * cartHeadNode.quantity; // accumulate the item price on the totalPrice
+            cartHeadNode = cartHeadNode.next; // get the next of the current itmNode
+            if (cartHeadNode.next == null) { // if the next is null, then it is a tail, get its value and break
+                totalPrice += cartHeadNode.next.price * cartHeadNode.next.quantity;
                 return totalPrice;
             }
         }
